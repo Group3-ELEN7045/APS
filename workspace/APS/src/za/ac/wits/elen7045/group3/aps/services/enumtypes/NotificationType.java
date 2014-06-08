cedae96af1a76dc86585e4c5382d2c612c54b3eb
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-08 17:00:08 +0200

    APS Workspace
    This is the initial APS workspace commit

---
+++ workspace/APS/src/za/ac/wits/elen7045/group3/aps/services/enumtypes/NotificationType.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package za.ac.wits.elen7045.group3.aps.services.enumtypes;
+
+/**
+ * @author SilasMahlangu
+ *
+ */
+public enum NotificationType {
+	SMS("SMS"), EMAIL("E"), DATABASE("DB"), LOGON("L");
+	 
+	private String notificationType;
+ 
+	private NotificationType(String notificationType) {
+		this.notificationType = notificationType;
+	}
+ 
+	public String getNotificationType() {
+		return notificationType;
+	}
+}
