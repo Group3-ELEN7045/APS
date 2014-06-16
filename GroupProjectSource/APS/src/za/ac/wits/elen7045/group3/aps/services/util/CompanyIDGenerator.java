78e6b6ea078defe6605d96d73c447dfa66179c80
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-16 15:25:29 +0200

    Deleting Workspace because of commited Conflicts

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/util/CompanyIDGenerator.java
+++
@@ -1,9 +0,0 @@
-package za.ac.wits.elen7045.group3.aps.services.util;
-import java.util.Random;
-public class CompanyIDGenerator {
-	static long range = 1234567L;
-	public static long generateCompanyID(){
-		return ((long)(new Random().nextDouble()*range));
-	}
-
-}
