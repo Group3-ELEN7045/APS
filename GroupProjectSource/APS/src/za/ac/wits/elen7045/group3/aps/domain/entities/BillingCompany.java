4395f1184f9a885f3f324f33f2b64c60e0808026
bakwanyana <bmthobela@gmail.com>
2014-06-18 18:57:34 +0200

    modified billing company to hold billing account (list)

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/entities/BillingCompany.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/entities/BillingCompany.java
@@ -1,7 +1,8 @@
 package za.ac.wits.elen7045.group3.aps.domain.entities;
 
 import java.io.Serializable;
-
+import java.util.List;
+import za.ac.wits.elen7045.group3.aps.services.enumtypes.ComanyStatementType;
 
 /**
  * @author Livious
@@ -9,9 +10,36 @@ import java.io.Serializable;
  */
 
 public class BillingCompany implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String companyName;
 	private String url;
-		
+	private List<BillingAccount> billingAccounts; 
+	private ComanyStatementType companyType;
+	
+	public BillingCompany(String companyName){
+		this.companyName = companyName;
+	}
+	
+	public List<BillingAccount> getBillingAccounts() {
+		return billingAccounts;
+	}
+	public void setBillingAccounts(List<BillingAccount> billingAccounts) {
+		this.billingAccounts = billingAccounts;
+	}
+	
+	public void addBillingAccounts(BillingAccount billingAccount){
+		billingAccounts.add(billingAccount);
+	}
+	public ComanyStatementType getCompanyType() {
+		return companyType;
+	}
+	public void setCompanyType(ComanyStatementType companyType) {
+		this.companyType = companyType;
+	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/enumtypes/ComanyStatementType.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/enumtypes/ComanyStatementType.java
@@ -7,12 +7,12 @@ package za.ac.wits.elen7045.group3.aps.services.enumtypes;
  * @author BakwanyanaThobela
  *
  */
-public enum AccountType {
+public enum ComanyStatementType {
 	MUNICIPALITY("M"), CREDITCARD("C"), TELCO("T");
 	 
 	private String accountType;
  
-	private AccountType(String accountType) {
+	private ComanyStatementType(String accountType) {
 		this.accountType = accountType;
 	}
  
