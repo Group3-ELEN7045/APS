e73cff11d1c8c354402af9598c46965c91b8faa0
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-18 18:42:00 +0200

    Deleted non used files

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/managers/BillingAccountStatementManager.java
+++
@@ -1,9 +0,0 @@
-package za.ac.wits.elen7045.group3.aps.services.managers;
-
-import java.util.List;
-
-import za.ac.wits.elen7045.group3.aps.domain.entities.BillingAccount;
-
-public interface BillingAccountStatementManager {
-	public List<BillingAccountStatement> getUserAggrigatedStatement(List<BillingAccount> billingAccountList, String billingPeriod);
-}
