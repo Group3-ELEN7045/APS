2088c6e26cd887f6a28fb5a263d77e59ac97c974
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-18 18:52:47 +0200

    Removed code thet refernced refactored classes

--- GroupProjectSource/APS/test/test/za/ac/group3/accounts/test/AccountDataRepositoryBasicTest.java
+++ GroupProjectSource/APS/test/test/za/ac/group3/accounts/test/AccountDataRepositoryBasicTest.java
@@ -1,32 +1,3 @@
 package test.za.ac.group3.accounts.test;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import za.ac.wits.elen7045.group3.aps.domain.accounts.abtracts.AbstractBillingAccountStatement;
-import za.ac.wits.elen7045.group3.aps.domain.accounts.repository.AccountRepository;
-import za.ac.wits.elen7045.group3.aps.domain.accounts.statement.MunicipalStatement;
-import za.ac.wits.elen7045.group3.aps.services.managers.StatementManager;
-
-
-public class AccountDataRepositoryBasicTest {
-
-	StatementManager adm;
-	
-	@Before
-	public void setUp(){
-		adm = new StatementManager(new AccountRepository());
-	}
-
-	@Test
-	public void test() {
-		
-		AbstractBillingAccountStatement  account = new MunicipalStatement("123456");
-		adm.addCustomerAccount(0, account);
-		
-		assertEquals(1, adm.getAccounts().size());
-	}
-
-}
+public class AccountDataRepositoryBasicTest {}
