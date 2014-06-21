03421656c32178b9fde676539e6fa64ad089c49c
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-21 20:18:41 +0200

    JPA Billing Account Repository

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/accounts/repository/BillingAccountRepositoryImpl.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/accounts/repository/BillingAccountRepositoryImpl.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import za.ac.wits.elen7045.group3.aps.domain.BillingAccountDataAccess;
 import za.ac.wits.elen7045.group3.aps.domain.entities.BillingAccount;
+import za.ac.wits.elen7045.group3.aps.domain.entities.BillingCompany;
 import za.ac.wits.elen7045.group3.aps.services.exception.DatabaseException;
-
-
 
 /**
  * @author Livious
@@ -15,42 +14,30 @@ import za.ac.wits.elen7045.group3.aps.services.exception.DatabaseException;
 
 public class BillingAccountRepositoryImpl implements BillingAccountRepository {
 	private BillingAccountDataAccess dataAccess;
-		
+
 	public BillingAccountRepositoryImpl(BillingAccountDataAccess dataAccess){
 		this.dataAccess = dataAccess;
 	}
 
 	@Override
-	public void saveBillingAccount(BillingAccount billingAccount)
-			throws DatabaseException {
-		dataAccess.saveBillingAccount(billingAccount);
+	public boolean saveBillingAccount(BillingAccount billingAccount)throws DatabaseException {
+
+		return dataAccess.saveBillingAccount(billingAccount);
 	}
 
 	@Override
-	public void updateBillingAccount(BillingAccount billingAccount)
-			throws DatabaseException {
-		dataAccess.updateBillingAccount(billingAccount);
-		
+	public boolean updateBillingAccountStatus(BillingAccount billingAccount)throws DatabaseException {
+
+		return dataAccess.updateBillingAccountStatus(billingAccount);
 	}
 
 	@Override
-	public BillingAccount getBillingAccount(String accountNumber)
-			throws DatabaseException {
-		return dataAccess.getBillingAccount(accountNumber);
+	public BillingAccount getBillingAccount(BillingAccount accountNumberSearch)	throws DatabaseException {
+		return dataAccess.getBillingAccount(accountNumberSearch);
 	}
 
 	@Override
-	public List<BillingAccount> getBillingAccounts(String billingCompanyName)
-			throws DatabaseException {
-		dataAccess.getBillingAccounts(billingCompanyName);
-		return null;
+	public List<BillingAccount> getBillingAccountsByCompanyName(BillingCompany billingCompany) throws DatabaseException {
+		return dataAccess.getBillingAccountsByCompanyName(billingCompany);
 	}
-
-	@Override
-	public BillingAccount getBillingStatement(String accountNumber,
-			String period) throws DatabaseException {		
-		return dataAccess.getBillingAccountStatement(accountNumber, period);
-	}
-	
-		
 }
