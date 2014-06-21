afe3cdd2d67198556dafa43cb1b4c32a1aa6328e
Boitumelo Mahlong <bmahlong@gmail.com>
2014-06-21 11:11:42 +0200

    update model

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/accounts/repository/BillingAccountStatementRepository.java
+++
@@ -1,7 +0,0 @@
-package za.ac.wits.elen7045.group3.aps.domain.accounts.repository;
-
-import za.ac.wits.elen7045.group3.aps.domain.entities.BillingAccountStatement;
-
-public interface BillingAccountStatementRepository {
-	public BillingAccountStatement getCustomerStatement(long billingAccountId, String billingPeriod);
-}
