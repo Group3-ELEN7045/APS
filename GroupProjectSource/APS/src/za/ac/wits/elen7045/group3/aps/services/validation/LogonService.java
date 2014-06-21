e32b0d4a74f1a317933203f999ba7522db95c0e9
bakwanyana <bmthobela@gmail.com>
2014-06-21 17:26:02 +0200

    modified logonservice

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/validation/LogonService.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/validation/LogonService.java
@@ -9,36 +9,6 @@ import za.ac.wits.elen7045.group3.aps.services.specification.ApplicationSpecific
 import za.ac.wits.elen7045.group3.aps.services.specification.user.UserAuthenticationSpecification;
 import za.ac.wits.elen7045.group3.aps.services.util.ApplicationContants;
 
-public class LogonService {
-	private CustomerRepository customerRepository;
-	private User user;
-	
-	public LogonService(CustomerRepository customerRepository ){
-		this.customerRepository = customerRepository;
-	}
-	
-	public Boolean validation(CredentialsVO credentials) throws Exception{
-					
-		user = customerRepository.getCustomer(credentials);
-		
-		if(user == null){			
-			throw new LogonException(ApplicationContants.USER_NOT_FOUND);
-		}
-		else
-		{
-			return authenitacte(user, credentials);
-		}
-	}
-	
-	private Boolean authenitacte(User user, CredentialsVO credentials) throws LogonException, Exception {
-		
-		CredentialsVO encryptedCredentials = user.getCredentials();	
-		
-		ApplicationSpecification<CredentialsVO> userAuthenticationSpec = new UserAuthenticationSpecification(credentials);
-		if(userAuthenticationSpec.isSatisfiedBy(encryptedCredentials)){
-			return true;
-		}
-		throw new LogonException(ApplicationContants.USER_CREDENTIALS_INCORRECT);
-	}
-		
+public interface LogonService {
+	public Boolean validation(CredentialsVO credentials) throws Exception;
 }
