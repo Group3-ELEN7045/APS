944df73083eca7783511cc7339ab48c56be25e9d
RonMenya <ronmenya@gmail.com>
2014-06-21 14:30:32 +0200

    Added MaintenanceWindow and updated TestCase

--- GroupProjectSource/APS/test/test/za/ac/wits/elen7045/group3/scheduler/TestMaintainaceWindow.java
+++ GroupProjectSource/APS/test/test/za/ac/wits/elen7045/group3/scheduler/TestMaintainaceWindow.java
@@ -1,10 +1,15 @@
 package test.za.ac.wits.elen7045.group3.scheduler;
 
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import za.ac.wits.elen7045.group3.aps.domain.scheduler.BillingCycle;
+import za.ac.wits.elen7045.group3.aps.domain.scheduler.MaintenanceWindow;
 import junit.framework.TestCase;
 
 
@@ -42,8 +47,38 @@ public class TestMaintainaceWindow extends TestCase {
 	 * @throws Exception
 	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testMaintenanceWindowGettersSetters() {
+		MaintenanceWindow maintenanceWindow = new MaintenanceWindow();
+		Calendar calendar = Calendar.getInstance();
+		long startTime = calendar.getTimeInMillis();
+		maintenanceWindow.setStartTime(startTime);
+		assertTrue(maintenanceWindow.getStartTime() == startTime);
+		calendar.add(Calendar.DATE, 2);
+		long endTime = calendar.getTimeInMillis();
+		maintenanceWindow.setEndTime(endTime);
+		assertTrue(maintenanceWindow.getEndTime() == endTime);
+		assertFalse(maintenanceWindow.getStartTime() == maintenanceWindow.getEndTime());
 	}
-
+	
+	/**
+	 * 
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testMaintenanceWindowsEquality() {
+		MaintenanceWindow maintenanceWindow1 = new MaintenanceWindow();
+		MaintenanceWindow maintenanceWindow2 = new MaintenanceWindow();
+		assertTrue(maintenanceWindow1.equals(maintenanceWindow2));
+		Calendar calendar =  Calendar.getInstance();
+		long startTime1 = calendar.getTimeInMillis();
+		maintenanceWindow1.setStartTime(startTime1);
+		maintenanceWindow2.setStartTime(startTime1);
+		calendar.add(Calendar.DAY_OF_WEEK, 2);
+		long endTime = calendar.getTimeInMillis();
+		maintenanceWindow1.setEndTime(endTime);
+		calendar.add(Calendar.DAY_OF_WEEK, 3);
+		maintenanceWindow2.setEndTime(calendar.getTimeInMillis());
+		assertFalse(maintenanceWindow1.equals(maintenanceWindow2));
+	}
 }
