a48a07b75379ff7e1b33690267c0e9859f8d2bc8
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-22 00:23:29 +0200

    After Adding Derby

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/BillingAccountDataAccess.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/BillingAccountDataAccess.java
@@ -3,12 +3,13 @@ package za.ac.wits.elen7045.group3.aps.domain;
 import java.util.List;
 
 import za.ac.wits.elen7045.group3.aps.domain.entities.BillingAccount;
+import za.ac.wits.elen7045.group3.aps.domain.entities.BillingCompany;
 import za.ac.wits.elen7045.group3.aps.services.exception.DatabaseException;
 
 public interface BillingAccountDataAccess {
-	public void saveBillingAccount(BillingAccount billingAccount) throws DatabaseException;
-	public void updateBillingAccount(BillingAccount billingAccount) throws DatabaseException;
-	public BillingAccount getBillingAccount(String accountNumber) throws DatabaseException;
-	public List<BillingAccount> getBillingAccounts(String billingCompanyName) throws DatabaseException;
-	public BillingAccount getBillingAccountStatement(String accountNumber, String period) throws DatabaseException;
+	public boolean saveBillingAccount(BillingAccount billingAccount) throws DatabaseException;
+	public boolean updateBillingAccountStatus(BillingAccount billingAccount) throws DatabaseException;
+	public BillingAccount getBillingAccount(BillingAccount accountNumberSearch) throws DatabaseException;
+	public List<BillingAccount> getBillingAccountsByCompanyName(BillingCompany billingCompany) throws DatabaseException;
+	//public List<BillingAccount> getBillingAccountStatementByAccountNumberAndPeriod(BillingAccount accountNumberPeriodSeach) throws DatabaseException;
 }
--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/ScrapeLogResultDataAccess.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/ScrapeLogResultDataAccess.java
@@ -13,6 +13,6 @@ import za.ac.wits.elen7045.group3.aps.services.exception.DatabaseException;
  *
  */
 public interface ScrapeLogResultDataAccess {
-    public boolean updateNotification(ScrapeLogResult notification)throws DatabaseException;
-	public List<ScrapeLogResult> getNotifications(Long id,String status)throws DatabaseException;
+    public boolean updateScrapeLogResult(ScrapeLogResult notification)throws DatabaseException;
+	public List<ScrapeLogResult> getScrapeLogResults(ScrapeLogResult notification)throws DatabaseException;
 }
