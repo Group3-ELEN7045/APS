aba9017d0165a64e64e9a5dcdc4402ff0390a316
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-22 14:04:22 +0200

    Observer Pattern Refactoring

---
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/pattern/notification/observer/Observer.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package za.ac.wits.elen7045.group3.aps.services.pattern.notification.observer;
+
+/**
+ * @author SilasMahlangu
+ *
+ */
+public interface Observer {
+	
+	public void update(Object object);
+	
+	public void setSubject(Subject subject);
+
+}
---
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/pattern/notification/observer/Subject.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package za.ac.wits.elen7045.group3.aps.services.pattern.notification.observer;
+
+/**
+ * @author SilasMahlangu
+ *
+ */
+public interface Subject{
+    public void register(Observer observer);
+    public void unRegister(Observer observer);
+    
+    public void notifyObsrvers();
+    
+    public Object getResponse(Observer observer);
+    public void setResponse(Object object);
+}
