25172bb71b6e7541a441068d4fd76d3e130cf268
Boitumelo Mahlong <bmahlong@gmail.com>
2014-06-22 20:39:10 +0200

    intialn revision

---
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/scraper/interfaces/ScrapeAdaptor.java
@@ -0,0 +1,8 @@
+package za.ac.wits.elen7045.group3.aps.services.scraper.interfaces;
+
+import za.ac.wits.elen7045.group3.aps.domain.vo.CredentialsVO;
+import za.ac.wits.elen7045.group3.aps.services.scrape.ScrapeResult;
+
+public interface ScrapeAdaptor {
+	ScrapeResult scrapeWebsite(String url, CredentialsVO credenttials);
+}
