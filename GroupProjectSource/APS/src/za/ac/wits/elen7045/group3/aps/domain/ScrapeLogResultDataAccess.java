2db134adc6e84b54d68f3528246745d8e6436d5c
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-23 11:37:01 +0200

    Added New Method

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/ScrapeLogResultDataAccess.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/ScrapeLogResultDataAccess.java
@@ -3,7 +3,7 @@
  */
 package za.ac.wits.elen7045.group3.aps.domain;
 
-import java.util.List;
+import java.util.List; 
 
 import za.ac.wits.elen7045.group3.aps.domain.entities.ScrapeLogResult;
 import za.ac.wits.elen7045.group3.aps.services.exception.DatabaseException;
