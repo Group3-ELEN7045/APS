f54516467de2ba507fdbe4e24dcf7a4c4042b312
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-23 04:25:48 +0200

    Fixed overriden file

--- GroupProjectSource/APS/test/test/za/ac/wits/group3/register/tests/notifications/CheckNotifictionTest.java
+++ GroupProjectSource/APS/test/test/za/ac/wits/group3/register/tests/notifications/CheckNotifictionTest.java
@@ -1,9 +1,5 @@
-/**
- * 
- */
 package test.za.ac.wits.group3.register.tests.notifications;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -20,9 +16,8 @@ import za.ac.wits.elen7045.group3.aps.domain.entities.ScrapeLogResult;
 import za.ac.wits.elen7045.group3.aps.domain.repository.notification.ScrapeLogResultImpl;
 import za.ac.wits.elen7045.group3.aps.domain.repository.notification.ScrapeLogResultRepository;
 import za.ac.wits.elen7045.group3.aps.domain.vo.NotificationCheck;
-import za.ac.wits.elen7045.group3.aps.services.enumtypes.NotificationStatus;
-import za.ac.wits.elen7045.group3.aps.services.exception.DatabaseException;
-import za.ac.wits.elen7045.group3.aps.services.notification.observer.NotificationObserver;
+import za.ac.wits.elen7045.group3.aps.services.enumtypes.NotificationType;
+import za.ac.wits.elen7045.group3.aps.services.pattern.notification.observer.NotificationObserver;
 import za.ac.wits.elen7045.group3.aps.services.specification.ApplicationSpecification;
 import za.ac.wits.elen7045.group3.aps.services.specification.notification.CheckNotificationSpecification;
 
@@ -32,53 +27,41 @@ import za.ac.wits.elen7045.group3.aps.services.specification.notification.CheckN
  */
 public class CheckNotifictionTest {
 
-		private ScrapeLogResult               notification;
-		private List<ScrapeLogResult>         notifications;
-		private ScrapeLogResultDataAccess     notificationDataAccess;
-		private ScrapeLogResultRepository     notificationRepository;
-		private ScrapeLogResultImpl notificationRepositoryImpl;
-		private ApplicationContext         context;
-		
-		@Before
-		public void initilize(){
-			context                     = new  ClassPathXmlApplicationContext("res/spring/application-context-test.xml");
-			notification                = context.getBean(ScrapeLogResult.class);
-			notificationDataAccess      = context.getBean(ScrapeLogResultDataAccess.class);
-			notifications               = new ArrayList<ScrapeLogResult>();  
-			notificationRepositoryImpl  = new ScrapeLogResultImpl(notificationDataAccess);
-			notificationRepository      = new APSMockObjectGenerator<ScrapeLogResultImpl>().mock(notificationRepositoryImpl);
-		}
-		
-		@Test
-		public void testInsertNotifiction() throws DatabaseException{
-			notification.setNotificationType("message");
-			notification.setStatsus(NotificationStatus.WAITING.getNotificationStatus());
-			notification.setMessage("Please Update email address");
-			assertTrue("Unble to add Notifiction in Datbse", notificationRepository.updateScrapeLogResult(notification));  
-		}
-		
-		@Test
-		public void testValidteInsert() throws DatabaseException{
-			List<ScrapeLogResult> notifications = notificationRepository.getScrapeLogResult(Long.valueOf(1), null);
-			assertEquals("The databse shoud have 2 entries", notifications.size() , 1);
-		}
-		
-		//Check Notifiction Test
-		@Test
-		public void checkNotifictionTest(){
-			NotificationCheck checkNotification = new NotificationCheck();
-			NotificationObserver notificationObserver = new NotificationObserver();  
-			ScrapeLogResult responseNotification = new ScrapeLogResult();
-			
-			checkNotification.setId(Long.valueOf(1));
-			checkNotification.setNotificationStatus(NotificationStatus.WAITING.getNotificationStatus());
-			
-			List<ScrapeLogResult> dbNotifications = new ArrayList<ScrapeLogResult>();
-			dbNotifications = notificationObserver.checkNotifications(checkNotification, notificationRepository);
-			
-			responseNotification = dbNotifications.get(0);
-			
-			ApplicationSpecification<ScrapeLogResult> notificationSpecification = new CheckNotificationSpecification(notification);
-		    assertTrue("No Notifications in a wait state", notificationSpecification.isSatisfiedBy(responseNotification));	
-		}
+	private ScrapeLogResult notification;
+	private List<ScrapeLogResult> notifications;
+	private ScrapeLogResultDataAccess notificationDataAccess;
+	private ScrapeLogResultRepository notificationRepository;
+	private ScrapeLogResultImpl notificationRepositoryImpl;
+	private ApplicationContext context;
+
+	@Before
+	public void initilize(){
+		context = new ClassPathXmlApplicationContext("res/spring/application-context-test.xml");
+		notification = context.getBean(ScrapeLogResult.class);
+		notificationDataAccess = context.getBean(ScrapeLogResultDataAccess.class);
+		notifications = new ArrayList<ScrapeLogResult>();
+		notificationRepositoryImpl = new ScrapeLogResultImpl(notificationDataAccess);
+		notificationRepository = new APSMockObjectGenerator<ScrapeLogResultImpl>().mock(notificationRepositoryImpl);
+
+
+	}
+
+	//Check Notifiction Test
+	@Test
+	public void checkNotifictionObserverTest(){
+		NotificationCheck checkNotification = new NotificationCheck();
+		checkNotification.setAccountNumber("123456789");
+		checkNotification.setNotificationStatus(NotificationType.LOGON.getNotificationType());
+
+		NotificationObserver notificationObserver = new NotificationObserver();
+		ScrapeLogResult responseNotification = new ScrapeLogResult();
+
+		List<ScrapeLogResult> dbNotifications = new ArrayList<ScrapeLogResult>();
+		dbNotifications = notificationObserver.checkNotifications(checkNotification, notificationRepository);
+
+		responseNotification = dbNotifications.get(0);
+
+		ApplicationSpecification<ScrapeLogResult> notificationSpecification = new CheckNotificationSpecification(notification);
+		assertTrue("Not logon Notifications", notificationSpecification.isSatisfiedBy(responseNotification));
+	}
 }
