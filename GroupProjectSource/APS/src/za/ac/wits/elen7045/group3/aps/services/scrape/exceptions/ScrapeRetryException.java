988a8cde402cb129e3184e57c98bdd21b2fc1f49
Boitumelo Mahlong <bmahlong@gmail.com>
2014-06-24 23:17:24 +0200

    Change-Id: If97ef80778c923e2319f869ae4a7647570302ab4 Signed-off-by: Boitumelo Mahlong <bmahlong@gmail.com>

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/scrape/exceptions/ScrapeRetryException.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/scrape/exceptions/ScrapeRetryException.java
@@ -5,6 +5,11 @@ package za.ac.wits.elen7045.group3.aps.services.scrape.exceptions;
  *
  */
 public class ScrapeRetryException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ScrapeRetryException(String message) {
 		super(message);
 	}
