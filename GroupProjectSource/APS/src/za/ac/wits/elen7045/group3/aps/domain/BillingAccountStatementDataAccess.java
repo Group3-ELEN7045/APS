5f3d1c32077e14a29e7f0b9be0d3fdaeb3f4eae3
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-25 16:29:49 +0200

    Statements Persistence

--- GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/BillingAccountStatementDataAccess.java
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/domain/BillingAccountStatementDataAccess.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import za.ac.wits.elen7045.group3.aps.domain.entities.BillingAccountStatement;
 
-public interface BillingAccountStatementDataAccess <T>{
-	public boolean saveBillingAccountStatement(T t);
-	public List<BillingAccountStatement> getBillingAccountStatements(String accountNumber, String biilingPeriod);
+public interface BillingAccountStatementDataAccess{
+	public boolean saveBillingAccountStatement(BillingAccountStatement statement);
+	public List<BillingAccountStatement> getBillingAccountStatements(String accountNumber);
 }
 
