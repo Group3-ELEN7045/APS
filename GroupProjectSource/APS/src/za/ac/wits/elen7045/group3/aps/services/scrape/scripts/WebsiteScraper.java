4402f6e85cd980339f042c406cf63948215a8a91
SilasMahlangu <silas_mahlangu@hotmail.com>
2014-06-25 16:53:20 +0200

    renamed the package

---
+++ GroupProjectSource/APS/src/za/ac/wits/elen7045/group3/aps/services/scrape/scripts/WebsiteScraper.java
@@ -0,0 +1,15 @@
+package za.ac.wits.elen7045.group3.aps.services.scrape.scripts;
+
+import za.ac.wits.elen7045.group3.aps.domain.scrape.vo.ScrapedResult;
+import za.ac.wits.elen7045.group3.aps.domain.vo.CredentialsVO;
+import za.ac.wits.elen7045.group3.aps.services.scrape.acl.XMLFileMarshall;
+
+/**
+ * @author bakwanyana
+ * @author boitumelo(refactoring)
+ */
+public class WebsiteScraper {
+	public static ScrapedResult scrapeWebsite(String url, CredentialsVO credentials){
+		return (ScrapedResult)new XMLFileMarshall().convertScrapedDataToObject(ScrapedResult.class, url);
+	}
+}
